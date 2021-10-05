365df641066f9f0c585b37100c94e640f4426e41
Ulf Hermjakob <ulf@isi.edu>
2021-10-04 21:10:22 -0700

    initial set up

---
+++ __main__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    from .wildebeest_normalize import main
+    main()
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import wildebeest
+from pathlib import Path
+
+from setuptools import setup, find_namespace_packages
+
+long_description = Path('README.md').read_text(encoding='utf-8', errors='ignore')
+
+classifiers = [  # copied from https://pypi.org/classifiers/
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Utilities',
+    'Topic :: Text Processing',
+    'Topic :: Text Processing :: General',
+    'Topic :: Text Processing :: Filters',
+    'Topic :: Text Processing :: Linguistic',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3 :: Only',
+]
+
+setup(
+    name='wildebeest',
+    version=wildebeest.__version__,
+    description=wildebeest.__description__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=classifiers,
+    python_requires='>=3.8',
+    url='https://github.com/uhermjakob/wildebeest',
+    download_url='https://github.com/uhermjakob/wildebeest',
+    platforms=['any'],
+    author='Ulf Hermjakob',
+    author_email='ulf@isi.edu',
+    packages=find_namespace_packages(exclude=['aux']),
+    keywords=['machine translation', 'datasets', 'NLP', 'natural language processing,'
+                                                        'computational linguistics'],
+    entry_points={
+        'console_scripts': [
+            'wb_norm=wildebeest.wildebeest_normalize:main'
+        ],
+    },
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+)
