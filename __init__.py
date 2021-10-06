5b97771cbe762ed68347375c897160ea30113258
Ulf Hermjakob <ulf@isi.edu>
2021-10-05 19:09:37 -0700

    First non-dummy

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
-"""Dummy init file for Python scripts wildebeest"""
+__version__ = '0.7.0'
+__description__ = '''This wildebeest script repairs common encoding errors, normalizes characters into their canonical form, maps digits and some punctuation to ASCII, deletes many non-printable characters and performs other repair, normalization and cleaning steps.'''
+last_mod_date = 'October 5, 2021'
