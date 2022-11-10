c750fb7d8c390a561114373dd82d967db91e7516
Ulf Hermjakob <ulf@isi.edu>
2022-11-10 13:43:37 -0800

    updated version, last_mod_date

--- wildebeest/__init__.py
+++ wildebeest/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.7.2'
+__version__ = '0.8.1'
 __description__ = '''This wildebeest script repairs common encoding errors, normalizes characters into their canonical form, maps digits and some punctuation to ASCII, deletes many non-printable characters and performs other repair, normalization and cleaning steps.'''
-last_mod_date = 'November 12, 2021'
+last_mod_date = 'November 10, 2022'
