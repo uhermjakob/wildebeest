661d5cb017aaeb58c1c4a14248fab21b16deb82e
Ulf Hermjakob <ulf@isi.edu>
2022-11-18 20:48:07 -0800

    Updated version, date

--- wildebeest/__init__.py
+++ wildebeest/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '0.9.0'
+__version__ = '0.9.2'
 __description__ = '''The wildebeest scripts investigate, repair and normalize a wide range of text file problems at the character level, e.g. encoding errors, normalization of characters into their canonical form, mapping digits and some punctuation to ASCII, deletion of some non-printable characters.'''
-last_mod_date = 'November 17, 2022'
+last_mod_date = 'November 18, 2022'
 from . import wb_normalize, wb_analysis
 __all__ = [wb_normalize, wb_analysis]
