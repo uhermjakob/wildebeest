2e8ca7e5b527d842d7780e125ba25ddfa9d0bf7d
Ulf Hermjakob <ulf@isi.edu>
2022-11-22 16:00:47 -0800

    Added description sentence to description string

--- wildebeest/__init__.py
+++ wildebeest/__init__.py
@@ -1,4 +1,5 @@
-r"""Wildebeest documentation: https://github.com/uhermjakob/wildebeest
+r"""The wildebeest scripts investigate, repair and normalize a wide range of text file problems at the character level, e.g. encoding errors, normalization of characters into their canonical form, mapping digits and some punctuation to ASCII, deletion of some non-printable characters.
+Wildebeest documentation: https://github.com/uhermjakob/wildebeest
 Main modules: wildebeest.wb_analysis, wildebeest.wb_normalize
 Argument help: wb_analysis.py -h, wb_normalize.py -h; or, alternatively: wb-ana -h, wb-norm -h"""
 __version__ = '0.9.2'
